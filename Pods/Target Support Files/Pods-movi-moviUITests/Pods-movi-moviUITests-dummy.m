7ccbc375df1696cd6faa197952c0f709ca55554d
dinhtv22 <dinhtv22@viettel.com.vn>
2023-10-30 09:27:59 +0700

    init commit

---
+++ Pods/Target Support Files/Pods-movi-moviUITests/Pods-movi-moviUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_movi_moviUITests : NSObject
+@end
+@implementation PodsDummy_Pods_movi_moviUITests
+@end
---
+++ movi/moviApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviApp.swift
+//  movi
+//
+//  Created by Dinh mac mini on 30/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct moviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
